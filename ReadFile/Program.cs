2093ccbf25d3a3036b11830d5989b4d6de6edb82
onifade-joshua <onifadejoshua15@gmail.com>
2023-09-19 03:05:17 +0100

    Add project files.

---
+++ ReadFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // How to read file using ReadAllText in System.IO; 
+            string  FilePath = @"C:\Users\joshua\Documents\sample.txt";
+            string text = File.ReadAllText(FilePath);
+            Console.WriteLine(text);
+        }
+    }
+}
